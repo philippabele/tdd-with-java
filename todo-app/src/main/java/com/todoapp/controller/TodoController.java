3039b22cdb9757420a32f8787e10356f3d07c178
isa <106517383+isaaaabel@users.noreply.github.com>
2024-06-10 02:02:54 +0200

    Update TodoController.java

--- todo-app/src/main/java/com/todoapp/controller/TodoController.java
+++ todo-app/src/main/java/com/todoapp/controller/TodoController.java
@@ -3,34 +3,75 @@ package com.todoapp.controller;
 import com.todoapp.model.Todo;
 import com.todoapp.model.TodoRequest;
 import com.todoapp.repository.TodoRepository;
+import com.todoapp.service.TodoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/todos")
 public class TodoController {
 
-    @Autowired
-    private TodoRepository todoRepository;
+    private final TodoService todoService;
 
-    @GetMapping
-    public List<Todo> getTodos() {
-        return todoRepository.findAll();
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
     }
 
     @PostMapping
-    public ResponseEntity<Todo> addTodo(@RequestBody TodoRequest todoRequest) {
+    public ResponseEntity<Todo> addTodo(@Valid @RequestBody TodoRequest todoRequest) {
         Todo todo = new Todo();
         todo.setTitle(todoRequest.getTitle());
         todo.setDescription(todoRequest.getDescription());
         todo.setDueDate(todoRequest.getDueDate());
         todo.setCompleted(todoRequest.isCompleted());
 
-        Todo savedTodo = todoRepository.save(todo);
+        Todo savedTodo = todoService.save(todo);
         return new ResponseEntity<>(savedTodo, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Todo> getTodos() {
+        return todoService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
+        Optional<Todo> todo = todoService.findById(id);
+        return todo.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable Long id, @Valid @RequestBody TodoRequest todoRequest) {
+        Optional<Todo> existingTodo = todoService.findById(id);
+
+        if (existingTodo.isPresent()) {
+            Todo todo = existingTodo.get();
+            todo.setTitle(todoRequest.getTitle());
+            todo.setDescription(todoRequest.getDescription());
+            todo.setDueDate(todoRequest.getDueDate());
+            todo.setCompleted(todoRequest.isCompleted());
+            Todo updatedTodo = todoService.save(todo);
+            return ResponseEntity.ok(updatedTodo);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTodoById(@PathVariable Long id) {
+        Optional<Todo> existingTodo = todoService.findById(id);
+        if (existingTodo.isPresent()) {
+            todoService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
